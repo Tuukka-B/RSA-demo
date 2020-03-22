a1d4068973ac737fdaad8b7dcc796f259bdcb4c3
N4927 <N4927@student.jamk.fi>
2020-03-22 18:28:47 +0200

    translated english to finnish

--- encrypt.py
+++ encrypt.py
@@ -1,7 +1,7 @@
 import math_backend
 
-primes = math_backend.createprimes()
-n = primes[0] * primes[1]
-phi = (primes[0] -1)* (primes[1] -1)
-e = math_backend_gcd(e_factor)
+alkuluvut = math_backend.luo_alkuluvut()
+n = alkuluvut[0] * alkuluvut[1]
+fii = (alkuluvut[0] -1)* (alkuluvut[1] -1)
+e = math_backend.valitse_e(fii)
 d = d*e = 1
