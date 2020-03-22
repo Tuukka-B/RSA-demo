5f8ba192525a47abe76d8f7c5c4dd3225fc1f967
Tuukka Bordi <M2296@student.jamk.fi>
2020-03-22 16:00:59 +0200

    Example code for createprimes

--- math_backend.py
+++ math_backend.py
@@ -8,7 +8,9 @@ def createprimes():
     primes = []
     for x in range(0, 2):
         primes.append(gmpy2.next_prime(random.randint(minrange, maxrange)))
-
+        if primes[x] == primes[x-1]:
+            primes.pop(-1)
+            x = x-1
     return primes
 
 
