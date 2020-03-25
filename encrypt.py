c2f585b8e6e67057157e6a31194c356a05e27099
Tuukka Bordi <M2296@student.jamk.fi>
2020-03-25 19:09:29 +0200

    Valitse e funktio

--- encrypt.py
+++ encrypt.py
@@ -4,7 +4,7 @@ alkuluvut = math_backend.luo_alkuluvut()
 n = alkuluvut[0] * alkuluvut[1]
 fii = (alkuluvut[0] - 1) * (alkuluvut[1] - 1)
 e = math_backend.valitse_e(fii)
-d = valitse_d(e, fii)
+d = math_backend.valitse_d(e, fii)
 
 print("n: ", n)
 print("fii: ", fii)
